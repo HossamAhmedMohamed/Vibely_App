c2efaa1355ed4850ccf54b56d4c3daf406bed217
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2025-02-07 04:35:27 +0200

    set_up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:social_media_app/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print(e.toString());
+  }
+  try {
+    await Supabase.initialize(
+      url: dotenv.env["SUPABASE_URL"].toString(),
+      anonKey: dotenv.env["SUPABASE_KEY"].toString(),
+    );
+  } catch (e) {
+    print(e.toString());
+  }
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print(e.toString());
+  }
   runApp(const MyApp());
 }
 
